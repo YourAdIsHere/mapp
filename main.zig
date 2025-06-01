d47f0aa1d6d3de9e285ca8b4f008c7c161340bb2
youradishere <ad@youradhe.re>
2025-06-01 15:43:03 -0700

    Refactor output formatting

--- main.zig
+++ main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 pub fn main() !void {
     var closed: usize = 0;
     var open: usize = 0;
+    std.debug.print("{0s: <7}{1s: <10}{2s: <20}\n", .{ "PORT", "STATE", "SERVICE" });
     for (1..1000) |port| {
         const stream = std.net.tcpConnectToAddress(std.net.Address.initIp4([4]u8{ 127, 0, 0, 1 }, @intCast(port))) catch |err| {
             switch (err) {
@@ -13,9 +14,11 @@ pub fn main() !void {
         };
         var buf: [100]u8 = undefined;
         open += 1;
-        std.debug.print("Port {any} OPEN\n", .{port});
-        std.debug.print("{0s: <7}{1s: <10}{2s: <20}\n", .{ "PORT", "STATE", "SERVICE" });
-        std.debug.print("{0any: <5}{1s: <5}{2?s: <20}\n", .{ port, "open", try stream.reader().readUntilDelimiterOrEof(&buf, '\n') });
+        std.debug.print("{0any: <7}{1s: <10}{2?s: <20}\n", .{ port, "open", try stream.reader().readUntilDelimiterOrEof(&buf, '\n') });
     }
     std.debug.print("Not shown: {any} closed tcp ports\n", .{closed});
+}
+
+pub fn usage() void {
+    std.debug.print("");
 }
